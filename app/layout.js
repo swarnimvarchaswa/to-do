c4824cf384ef4114b0b50500b9d239a8a8d16382
swarnimvarchaswa <2021031178@mmmut.ac.in>
2024-09-28 17:15:26 +0530

    final

--- app/layout.js
+++ app/layout.js
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="@/app/favicon.png" />
+        <link rel="icon" href="./favicon.png" />
       </head>
       <body className={roboto.className}>{children}</body>
     </html>
