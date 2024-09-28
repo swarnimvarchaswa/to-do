35f2cf3f57951b62cf44307126c0fb651712ac2f
swarnimvarchaswa <2021031178@mmmut.ac.in>
2024-09-28 17:09:14 +0530

    favicon

--- app/layout.js
+++ app/layout.js
@@ -2,17 +2,19 @@ import { Roboto_Flex } from "next/font/google";
 import "./globals.css";
 
 // const inter = Inter({ subsets: ["latin"] });
-const roboto = Roboto_Flex({subsets: ["latin"]});
+const roboto = Roboto_Flex({ subsets: ["latin"] });
 
 export const metadata = {
   title: "To Do",
   description: "To Do List App",
-  
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="@/app/favicon.png" />
+      </head>
       <body className={roboto.className}>{children}</body>
     </html>
   );
